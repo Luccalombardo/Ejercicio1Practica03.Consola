3f2a1be33ab5a31eb80b3f1fd14637f0c263e95e
Lucca <Lucca@LUCCA-PC>
2025-05-15 01:13:36 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio01/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Ejercicio01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Saber si el número positivo o negativo y si el mismo es par o impar");
+
+                Console.Write("Ingrese un numero: ");
+                int numero = int.Parse(Console.ReadLine());
+
+                if (numero >= 0)
+                {
+                    Console.WriteLine($"El numero {numero} es positivo");
+                }
+                else
+                {
+                    Console.WriteLine($"El numero {numero} es negativo");
+                }
+
+                Console.ReadKey();
+
+                if (numero % 2 == 0)
+                {
+                    Console.WriteLine($"El numero {numero} es par");
+                }
+                else
+                {
+                    Console.WriteLine($"El numero {numero} es impar");
+                }
+                Console.ReadKey();
+            }
+            catch (FormatException ex)
+            {
+
+                Console.WriteLine("ERROR: El formato ingresado es invalido"); ;
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("ERROR: Excepcion no manejada"); ;
+            }
+
+        }
+    }
+}
---
+++ Ejercicio02.Consola/Program.cs
@@ -0,0 +1,57 @@
+﻿
+namespace Ejercicio02.Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Ingresadas las coordenadas de un punto en el plano, informar en qué cuadrante se encuentra el mismo");
+                Console.Write("Ingrese el eje x del punto: ");
+                int X1 = int.Parse(Console.ReadLine());
+                Console.Write("Ingrese el eje Y del punto: ");
+                int Y1 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine($"El punto ({X1},{Y1}) está {SaberCuadrante(X1, Y1)}");
+            }
+            catch (FormatException ex)
+            {
+
+                Console.WriteLine("ERROR: El formato ingresado es invalido"); ;
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("ERROR: Excepcion no manejada"); ;
+            }
+
+
+        }
+
+        private static string SaberCuadrante(int x1, int y1)
+        {
+            if (x1 == 0 || y1 == 0)
+            {
+                return ($"sobre un eje y no pertenece a ningún cuadrante.");
+            }
+
+            else if (x1 > 0 && y1 > 0)
+            {
+                return ("en el primer cuadrante");
+            }
+            else if (x1 < 0 && y1 > 0)
+            {
+                return ("en el segundo cuadrante");
+            }
+            else if (x1 < 0 && y1 < 0)
+            {
+                return ("en el tercer cuadrante");
+            }
+            else
+            {
+                return ("en el cuarto cuadrante");
+            }
+        }
+    }
+}
